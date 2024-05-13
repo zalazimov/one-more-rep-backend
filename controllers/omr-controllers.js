d1c5b4f5aa4fd9b9e492d8aacd0f94e6f67016c8
zazimov <zalmanazimov@pursuit.org>
2024-05-12 23:24:26 -0400

    added controller with get exercises

---
+++ controllers/omr-controllers.js
@@ -0,0 +1,11 @@
+const express = require("express");
+// const router = express.Router()
+const { Router } = require("express");
+
+const exercisesController = Router();
+
+exercisesController.get("/", (req, res) => {
+  res.status(200).json({ data: exercises });
+});
+
+module.exports = exercisesController;
