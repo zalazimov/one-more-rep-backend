0091213d7f03bb35f2b50fcb8f7a55a966f1e01e
zazimov <zalmanazimov@pursuit.org>
2024-05-12 23:33:32 -0400

    Added controller and 404 route to app.js

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const cors = require("cors");
 const express = require("express");
 const morgan = require("morgan");
+const omr_controller = require("./controllers/omr_controllers.js");
 
+const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
-app.us(morgan("dev"));
+app.use(morgan("dev"));
 
+// Controller
+app.use("/exercises", omr_controller);
+
+// Health Check route
 app.get("/", (req, res) => {
   res.send("Welcome to OneMoreRep");
+});
+
+// 404 route
+app("*", (req, res) => {
+  res.status(400).send("Page not Found");
 });
 
 module.exports = app;
